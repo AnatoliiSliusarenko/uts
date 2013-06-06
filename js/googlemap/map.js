95f9f2cb921af7fb0a97ababb9d9f83b7157e2d1
oligarch777 <dj.slyusar@rambler.ru>
2013-06-06 14:42:04 +0300

    map

--- js/googlemap/map.js
+++ js/googlemap/map.js
@@ -1,11 +1,6 @@
-google.maps.visualRefresh = true;
-
-var map;
-
 function initMap(){
-	/*
 	var $mapHolder = $("#map-holder");
-	google.maps.visualRefresh = true;
+	$mapHolder.empty();
 	$mapHolder.gMap({
         controls: {
             panControl: true,
@@ -17,25 +12,11 @@ function initMap(){
         },
         scrollwheel: true,
     	address: $mapHolder.attr("data-address"),
-    	zoom: 1,
+    	zoom: 4,
     	markers:[{
     			latitude: $mapHolder.attr("data-latitude"),
     			longitude: $mapHolder.attr("data-longitude")
    		}]
-    });  */
-	
-	
-	var mapOptions = {
-		    zoom: 14,
-		    center: new google.maps.LatLng(48.618600, 22.298765),
-		    mapTypeId: google.maps.MapTypeId.ROADMAP
-		  };
-		  map = new google.maps.Map(document.getElementById('map-holder'),
-		      mapOptions);
-		  //google.maps.event.trigger(map, 'resize'); 
-		  setTimeout(function() {
-            google.maps.event.trigger(map,'resize');
-        }, 500);
+    });  
 		 
 }
-google.maps.event.addDomListener(window, 'load', initMap);

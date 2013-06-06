c790605306768177eabe59044a2fe9ab24d531cb
AndrewYanuta <andrew@Rubik>
2013-06-06 12:10:59 +0300

    Map fix

--- js/googlemap/map.js
+++ js/googlemap/map.js
@@ -17,7 +17,7 @@ function initMap(){
         },
         scrollwheel: true,
     	address: $mapHolder.attr("data-address"),
-    	zoom: 5,
+    	zoom: 1,
     	markers:[{
     			latitude: $mapHolder.attr("data-latitude"),
     			longitude: $mapHolder.attr("data-longitude")
@@ -26,12 +26,16 @@ function initMap(){
 	
 	
 	var mapOptions = {
-		    zoom: 8,
-		    center: new google.maps.LatLng(-34.397, 150.644),
+		    zoom: 14,
+		    center: new google.maps.LatLng(48.618600, 22.298765),
 		    mapTypeId: google.maps.MapTypeId.ROADMAP
 		  };
 		  map = new google.maps.Map(document.getElementById('map-holder'),
 		      mapOptions);
-		  google.maps.event.trigger(map, 'resize'); 
+		  //google.maps.event.trigger(map, 'resize'); 
+		  setTimeout(function() {
+            google.maps.event.trigger(map,'resize');
+        }, 500);
+		 
 }
 google.maps.event.addDomListener(window, 'load', initMap);
